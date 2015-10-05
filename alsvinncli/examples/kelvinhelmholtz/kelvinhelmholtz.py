c43121ae742f8b0cf508a05331475b989b261c79
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2015-10-05 09:40:30 +0200

    added random data to kh

--- alsvinncli/examples/kelvinhelmholtz/kelvinhelmholtz.py
+++ alsvinncli/examples/kelvinhelmholtz/kelvinhelmholtz.py
@@ -2,8 +2,16 @@
 GAMMA=5.0/3.0
 import random
 # instability
-perturbation = 0.001 * sin(100*2*pi*x)
-if y < 0.25 + perturbation or y > 0.75 + perturbation:
+if has_random_variables:
+	N = len(a1)
+	perturbation = 0.01
+	perturbation_upper = perturbation*sum([a1[i]*cos(2*pi*i*(x+b1[i])) for i in range(len(a1))])/N
+	perturbation_lower = perturbation*sum([a2[i]*cos(2*pi*i*(x+b2[i])) for i in range(len(a2))])/N
+else:
+	perturbation_upper = 0
+	perturbation_lower = 0
+
+if y < 0.25 + perturbation_lower or y > 0.75 + perturbation_upper:
     rho = 1
     ux = 0.5
     uy = 0
