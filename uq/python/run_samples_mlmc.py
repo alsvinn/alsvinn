b3bdfa964fe4b55341f4b68ffbe8337aaeb6221d
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2015-10-07 18:02:50 +0200

    run_samples_mlmc.py: fixed some bugs

--- uq/python/run_samples_mlmc.py
+++ uq/python/run_samples_mlmc.py
@@ -6,9 +6,9 @@ import os.path
 
 def runAlsvinn(alsvinnpath, alsmlmc, work):
     if len(work) == 1:
-        commandToRun = "%s %s" % (alsvinnpath, work)
+        commandToRun = "%s %s" % (alsvinnpath, work[0])
     else:
-        commandToRun "%s -f %s -c %s -s 0.05" % (alsmlmc, work[0], work[1])
+        commandToRun = "%s -f %s -c %s -s 0.05" % (alsmlmc, work[0], work[1])
         print("Running\n\t%s" % commandToRun)
         os.system(commandToRun)
 
@@ -32,7 +32,7 @@ import os
 currentDir = os.path.abspath(os.curdir)
 
 for work in nodes.workLists[rank]:
-    basepath = os.path.dirname(work)
+    basepath = os.path.dirname(work[0])
     os.chdir(basepath)
     runAlsvinn(alsvinnExecutable, alsmlmcExecutable, work)
 
