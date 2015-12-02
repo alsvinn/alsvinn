a5608f6aff52c6caf57185951c95b1d559d0fbd3
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2015-12-02 18:23:20 +0100

    added periodic boundary test

--- test/src/BoundaryTest.cpp
+++ test/src/BoundaryTest.cpp
@@ -168,3 +168,137 @@ TEST_F(BoundaryTest, NeumannTest2CellsVarying2D) {
     }
 }
 
+
+
+TEST_F(BoundaryTest, PeriodicTest2CellsConstant) {
+    boundaryFactory.reset(new BoundaryFactory("periodic", deviceConfiguration));
+
+    auto volume = volumeFactory->createConservedVolume(nx, ny, nz, ghostCells);
+    volume->makeZero();
+    const real C = 10;
+    fill_volume<equation::euler::ConservedVariables>(*volume, grid, [&](real x, real y, real z, equation::euler::ConservedVariables& out) {
+        out.E = C;
+        out.m.x = C;
+        out.m.y = C;
+        out.m.z = C;
+        out.rho = C;
+    });
+
+    auto boundary = boundaryFactory->createBoundary(ghostCells);
+
+    boundary->applyBoundaryConditions(*volume, grid);
+
+
+    for_each_cell_index(*volume, [&](size_t index) {
+        ASSERT_EQ(volume->getScalarMemoryArea("rho")->getPointer()[index], C);
+    });
+}
+
+
+TEST_F(BoundaryTest, PeriodicTest2CellsVarying) {
+    boundaryFactory.reset(new BoundaryFactory("periodic", deviceConfiguration));
+
+    auto volume = volumeFactory->createConservedVolume(nx, ny, nz, ghostCells);
+    volume->makeZero();
+
+    auto rho = volume->getScalarMemoryArea("rho")->getView();
+    auto mx = volume->getScalarMemoryArea("mx")->getView();
+    auto my = volume->getScalarMemoryArea("my")->getView();
+    auto mz = volume->getScalarMemoryArea("mz")->getView();
+    auto E = volume->getScalarMemoryArea("E")->getView();
+
+    for(size_t x = ghostCells; x < nx + ghostCells; ++x) { // NOTE: The endpoint is correct, this is indexing wrt. full volume, WITH ghost cells
+        for(size_t y = ghostCells; y < ny + ghostCells; ++y) { // NOTE: The endpoint is correct, this is indexing wrt. full volume, WITH ghost cells
+            for(size_t z = ghostCells; z < nz + ghostCells; ++z) { // NOTE: The endpoint is correct, this is indexing wrt. full volume, WITH ghost cells
+                rho.at(x, y, z) = rho.index(x, y, z);
+                mx.at(x, y, z) = rho.index(x, y, z);
+                my.at(x, y, z) = rho.index(x, y, z);
+                mz.at(x, y, z) = rho.index(x, y, z);
+                E.at(x, y, z) = rho.index(x, y, z);
+            }
+        }
+    }
+
+
+    auto boundary = boundaryFactory->createBoundary(ghostCells);
+
+    boundary->applyBoundaryConditions(*volume, grid);
+
+    // X side
+    for(size_t y = ghostCells; y < ny + ghostCells; ++y) {
+        for(size_t z = ghostCells; z < nz + ghostCells; ++z) {
+            ASSERT_EQ(rho.at(0, y, z), rho.index(nx - 2 + ghostCells, y, z));
+            ASSERT_EQ(rho.at(1, y, z), rho.index(nx - 1 + ghostCells, y, z));
+
+            ASSERT_EQ(rho.at(ghostCells + nx + 1, y, z), rho.index(3, y, z))
+                << "Wrong index at (" << (ghostCells + nx + 1) << ", " << y << ", " << z << ")";
+            ASSERT_EQ(rho.at(ghostCells + nx, y, z), rho.index(2, y, z))
+                << "Wrong index at (" << (ghostCells + nx) << ", " << y << ", " << z << ")";;
+        }
+    }
+
+    // Y side
+    for(size_t x = ghostCells; x < nx + ghostCells; ++x) {
+        for(size_t z = ghostCells; z < nz + ghostCells; ++z) {
+            ASSERT_EQ(rho.at(x, 0, z), rho.index(x, ny - 2+ ghostCells, z));
+            ASSERT_EQ(rho.at(x, 1, z), rho.index(x, ny - 1+ ghostCells, z));
+
+            ASSERT_EQ(rho.at(x, ghostCells + ny + 1, z), rho.index(x, 3, z));
+            ASSERT_EQ(rho.at(x, ghostCells + ny, z), rho.index(x, 2, z));
+        }
+    }
+
+    // Z side
+    for(size_t x = ghostCells; x < nx + ghostCells; ++x) {
+        for(size_t y = ghostCells; y < ny + ghostCells; ++y) {
+            ASSERT_EQ(rho.at(x, y, 0), rho.index(x, y, nz - 2+ ghostCells));
+            ASSERT_EQ(rho.at(x, y, 1), rho.index(x, y, nz - 1+ ghostCells));
+
+            ASSERT_EQ(rho.at(x, y, ghostCells + nz + 1), rho.index(x, y, 3));
+            ASSERT_EQ(rho.at(x, y, ghostCells + nz), rho.index(x, y, 2));
+        }
+    }
+}
+
+
+
+TEST_F(BoundaryTest, PeriodicTest2CellsVarying2D) {
+    boundaryFactory.reset(new BoundaryFactory("periodic", deviceConfiguration));
+
+    auto volume = volumeFactory->createConservedVolume(nx, ny, 1, ghostCells);
+    volume->makeZero();
+
+    auto rho = volume->getScalarMemoryArea("rho")->getView();
+    auto mx = volume->getScalarMemoryArea("mx")->getView();
+    auto my = volume->getScalarMemoryArea("my")->getView();
+    auto mz = volume->getScalarMemoryArea("mz")->getView();
+    auto E = volume->getScalarMemoryArea("E")->getView();
+
+    size_t z = 0;
+    for(size_t x = ghostCells; x < nx + ghostCells; ++x) {
+        for(size_t y = ghostCells; y < ny + ghostCells; ++y) {
+
+            rho.at(x, y, z) = rho.index(x, y, z);
+            mx.at(x, y, z) = rho.index(x, y, z);
+            my.at(x, y, z) = rho.index(x, y, z);
+            mz.at(x, y, z) = rho.index(x, y, z);
+            E.at(x, y, z) = rho.index(x, y, z);
+
+        }
+    }
+
+
+    auto boundary = boundaryFactory->createBoundary(ghostCells);
+
+    auto smallerGrid = alsfvm::make_shared<grid::Grid>(rvec3(0,0,0), rvec3(1, 1,0), ivec3(nx, ny, 1));
+    boundary->applyBoundaryConditions(*volume, *smallerGrid);
+
+    // X side
+    for(size_t y = ghostCells; y < ny + ghostCells; ++y) {
+        ASSERT_EQ(rho.at(0, y, z), rho.index(nx - 2+ ghostCells, y, z));
+        ASSERT_EQ(rho.at(1, y, z), rho.index(nx - 1+ ghostCells, y, z));
+
+        ASSERT_EQ(rho.at(ghostCells + nx + 1, y, z), rho.index(3, y, z));
+        ASSERT_EQ(rho.at(ghostCells + nx, y, z), rho.index(2, y, z));
+    }
+}
