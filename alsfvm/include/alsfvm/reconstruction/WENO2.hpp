7cee241e1ea977d20eb372952fe85a2e4750a97c
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2015-12-02 09:39:55 +0100

    Corrected right/left coefficients.

--- alsfvm/include/alsfvm/reconstruction/WENO2.hpp
+++ alsfvm/include/alsfvm/reconstruction/WENO2.hpp
@@ -25,34 +25,39 @@ namespace alsfvm { namespace reconstruction {
             const real b0 = square(eq.getWeight(in, indexRight) - i0);
             const real b1 = square(i0 - eq.getWeight(in, indexLeft));
 
-            const real a0 = 1 / (3 * (ALSFVM_WENO_EPSILON + b0)*(ALSFVM_WENO_EPSILON + b0));
-            const real a1 = 2 / (3 * (ALSFVM_WENO_EPSILON + b1)*(ALSFVM_WENO_EPSILON + b1));
-            const real w0 = a0 / (a0 + a1);
-            const real w1 = a1 / (a0 + a1);
+            const real a0Left = 1 / (3 * (ALSFVM_WENO_EPSILON + b0)*(ALSFVM_WENO_EPSILON + b0));
+            const real a1Left = 2 / (3 * (ALSFVM_WENO_EPSILON + b1)*(ALSFVM_WENO_EPSILON + b1));
+            const real w0Left = a0Left / (a0Left + a1Left);
+            const real w1Left = a1Left / (a0Left + a1Left);
 
+            const real a0Right = 2/(3*(ALSFVM_WENO_EPSILON + b0)*(ALSFVM_WENO_EPSILON + b0));
+            const real a1Right = 1/(3*(ALSFVM_WENO_EPSILON + b1)*(ALSFVM_WENO_EPSILON + b1));
+            const real w0Right = a0Right / (a0Right + a1Right);
+            const real w1Right = a1Right / (a0Right + a1Right);
+#if 1
 #pragma unroll
             for (size_t var = 0; var < Equation::getNumberOfConservedVariables(); ++var) {
-                leftView.get(var).at(indexOut) = 0.5*(w1 * in.get(var).at(indexLeft) +
-                                        (3 * w0 + w1) * in.get(var).at(indexOut) -
-                                        w0 * in.get(var).at(indexRight));
+                leftView.get(var).at(indexOut) = 0.5*(w1Left * in.get(var).at(indexLeft) +
+                                        (3 * w0Left + w1Left) * in.get(var).at(indexOut) -
+                                        w0Left * in.get(var).at(indexRight));
 
-                rightView.get(var).at(indexOut) = 0.5*(w0 * in.get(var).at(indexRight) +
-                    (3 * w1 + w0) * in.get(var).at(indexOut) -
-                    w1 * in.get(var).at(indexLeft));
+                rightView.get(var).at(indexOut) = 0.5*(w0Right * in.get(var).at(indexRight) +
+                    (3 * w1Right + w0Right) * in.get(var).at(indexOut) -
+                    w1Right * in.get(var).at(indexLeft));
 
             }
-#if 0
+#else
             typename Equation::ConservedVariables inLeft = eq.fetchConservedVariables(in, indexLeft);
             typename Equation::ConservedVariables inMiddle = eq.fetchConservedVariables(in, indexOut);
             typename Equation::ConservedVariables inRight = eq.fetchConservedVariables(in, indexRight);
 
-            typename Equation::ConservedVariables left =  0.5*(w1 * inLeft +
-                                                               (3 * w0 + w1) * inMiddle -
-                                                               w0 * inRight);
+            typename Equation::ConservedVariables left =  0.5*(w1Left * inLeft +
+                                                               (3 * w0Left + w1Left) * inMiddle -
+                                                               w0Left * inRight);
 
-            typename Equation::ConservedVariables right =  0.5*(w0 * inRight +
-                                                                (3 * w1 + w0) * inMiddle -
-                                                                w1 * inLeft);
+            typename Equation::ConservedVariables right =  0.5*(w0Right * inRight +
+                                                                (3 * w1Right + w0Right) * inMiddle -
+                                                                w1Right * inLeft);
             eq.setViewAt(leftView, indexOut, left);
             eq.setViewAt(rightView, indexOut, right);
 #endif
