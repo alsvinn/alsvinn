592ea94f4e2e740fd035727ab5b0de3fb116de63
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2016-04-01 18:14:59 +0200

    updated normalization for kh

--- alsvinncli/examples/kelvinhelmholtz/kelvinhelmholtz.py
+++ alsvinncli/examples/kelvinhelmholtz/kelvinhelmholtz.py
@@ -1,13 +1,20 @@
 
-GAMMA=5.0/3.0
+GAMMA=1.4
 import random
 # instability
 
 if "has_random_variables" in locals() and has_random_variables:
 	N = len(a1)
-	perturbation = 0.1
-	perturbation_upper = perturbation*sum([a1[i]*cos(2*pi*i*(x+b1[i])) for i in range(len(a1))])/N
-	perturbation_lower = perturbation*sum([a2[i]*cos(2*pi*i*(x+b2[i])) for i in range(len(a2))])/N
+	perturbation = 0.01
+	normalization1 = sum(a1)
+	if abs(normalization1) < 1e-10:
+		normalization1 = 1
+	normalization2 = sum(a2)
+	if abs(normalization2) < 1e-10:
+		normalization2 = 1
+
+	perturbation_upper = perturbation*sum([a1[i]*cos(2*pi*i*(x+b1[i])) for i in range(len(a1))])/normalization1
+	perturbation_lower = perturbation*sum([a2[i]*cos(2*pi*i*(x+b2[i])) for i in range(len(a2))])/normalization2
 else:
 	perturbation_upper = 0
 	perturbation_lower = 0
