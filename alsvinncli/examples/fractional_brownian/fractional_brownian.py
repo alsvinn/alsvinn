91b16053dfe6b8beed0d9b5c64d5aa2c19ecc9b6
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2017-02-21 21:09:43 +0100

    Added new initial data

---
+++ alsvinncli/examples/fractional_brownian/fractional_brownian.py
@@ -0,0 +1,29 @@
+# This is found in the paper
+# Brouste, A., Istas, J., & Lambert-Lacroix, S. (2007).
+#  On Fractional Gaussian Random Fields Simulations. Journal of Statistical Software, 23(1), 1–23.
+#  http://doi.org/http://dx.doi.org/10.18637/jss.v023.i01
+# in the section 2.3
+def init_global(u, nx, ny, nz):
+    H = 0.01
+    M = u.shape[0]
+    N = M+1
+
+    K = log2(M)
+    B = zeros(N)
+    B[-1] = X[len(X)-1]
+    n=1
+    random_counter = 0
+    for k in range(int(K),0,-1):
+        for j in range(0, int(M/2**k)):
+
+            index = 2**(k-1)*(2*j+1)
+            left = index - 2**(k-1)
+            right = index + 2**(k-1)
+            B[index] = 0.5*(B[left]+B[right])+sqrt((1-2**(2*H-2))/2**(2*n*H))*X[random_counter]
+            random_counter += 1
+        n+=1
+
+    u[:,0,0]= B[:-1]
+
+
+
