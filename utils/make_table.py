071d6ea173e02a87899b5ee33e49d805af8710ec
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2017-02-23 17:40:42 +0100

    Fixed some small things with printing script

--- utils/make_table.py
+++ utils/make_table.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
 
     fields = {"equation":"Equation",
               "reconstruction":"Reconstruction", "integrator":"Time integrator", "cfl": "CFL", "endTime":"$T$","flux":"Numerical flux"}
+
+    fieldNames = ["equation", "flux", "reconstruction", "integrator",  "cfl", "endTime"]
     fvm = f.getElementsByTagName("config")[0].getElementsByTagName('fvm')[0]
 
     print("\\begin{table}\n")
@@ -27,7 +29,7 @@ if __name__ == "__main__":
     for field in fields.keys():
         sys.stdout.write("l")
     sys.stdout.write("}\n \\toprule\n")
-    for (n,field) in enumerate(fields.keys()):
+    for (n,field) in enumerate(fieldNames):
 
         sys.stdout.write ("%s" % fields[field])
 
@@ -39,7 +41,7 @@ if __name__ == "__main__":
     print("\midrule")
     usedValues = {}
 
-
+    
     for (n,field) in enumerate(fields.keys()):
         value = fvm.getElementsByTagName(field)[0].firstChild.nodeValue
         usedValues[field] = value
@@ -58,7 +60,7 @@ if __name__ == "__main__":
 
     prettyNames = {'forwardeuler':'Forward-Euler', 'rungekutta2':'SSP RK 2', "burgers":"Burgers", "euler1":"1D Euler", "euler2":"2D Euler", "euler3":"3D Euler"}
 
-    for (n,field) in enumerate(fields.keys()):
+    for (n,field) in enumerate(fieldNames):
         value = usedValues[field]
         if value in prettyNames.keys():
             
