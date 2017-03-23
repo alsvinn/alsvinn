87c37243f4562f905b1e683e6c7307362ac8cf9a
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2017-03-23 19:47:31 +0100

    Fixed pointer bug

--- alsuq/src/stats/StatisticsFactory.cpp
+++ alsuq/src/stats/StatisticsFactory.cpp
@@ -44,6 +44,8 @@ StatisticsFactory::StatisticsPointer StatisticsFactory::makeStatistics(const std
     }
     StatisticsPointer pointer;
     pointer.reset(new StatisticsTimer(name, list[platform][name](params)));
+
+    return pointer;
 }
 
 }
