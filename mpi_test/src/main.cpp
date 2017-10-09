dbd5221d6c382d743698c6b91b12671561ee2cd9
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2017-10-09 19:53:06 +0200

    Added gpu direct support for unitestting

--- mpi_test/src/main.cpp
+++ mpi_test/src/main.cpp
@@ -3,13 +3,38 @@
 #ifdef ALSVINN_USE_MPI
 #include <mpi.h>
 #endif
-
+#include <cuda_runtime.h>
+#include <cstdlib>
 int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
 #ifdef ALSVINN_USE_MPI
-    MPI_Init(&argc, &argv);
+  setenv("MPICH_RDMA_ENABLED_CUDA", "1", 1);
+  setenv("MV2_USE_CUDA", "1", 1);
+  MPI_Init(NULL, NULL);
+
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    double* a;
+    cudaMalloc(&a, sizeof(a));
+    if(size>1) {
+      MPI_Request requestSend;
+      MPI_Isend(a,1, MPI_DOUBLE, (rank+1)%size, 0, MPI_COMM_WORLD,
+                &requestSend);
+      MPI_Request requestReceive;
+      MPI_Irecv(a,1, MPI_DOUBLE, (rank+1)%size, 0, MPI_COMM_WORLD,
+                &requestReceive);
+      MPI_Wait(&requestSend, MPI_STATUS_IGNORE);
+      MPI_Wait(&requestReceive, MPI_STATUS_IGNORE);
+
+    }
+
 #endif
 
+
+
+	::testing::InitGoogleTest(&argc, argv);
 
     auto exitValue= RUN_ALL_TESTS();
     MPI_Finalize();
