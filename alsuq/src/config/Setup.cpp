ebd875c3020c0bda25ed47b08c6b97b793f8ee57
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-05-24 16:44:59 +0200

    Removed well generator because of licensing issues

--- alsuq/src/config/Setup.cpp
+++ alsuq/src/config/Setup.cpp
@@ -3,12 +3,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -104,7 +104,7 @@ std::shared_ptr<samples::SampleGenerator> Setup::makeSampleGenerator(
     auto generatorName = configuration.get<std::string>("uq.generator");
 
     if (generatorName == "auto") {
-        generatorName = "well512a";
+        generatorName = "stlmersenne";
     }
 
     auto parametersNode = configuration.get_child("uq.parameters");
--- alsuq/src/generator/GeneratorFactory.cpp
+++ alsuq/src/generator/GeneratorFactory.cpp
@@ -3,19 +3,18 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "alsuq/generator/GeneratorFactory.hpp"
 #include "alsuq/generator/STLMersenne.hpp"
-#include "alsuq/generator/Well512A.hpp"
 #include "alsutils/error/Exception.hpp"
 
 namespace alsuq {
@@ -29,8 +28,6 @@ std::shared_ptr<Generator> GeneratorFactory::makeGenerator(
 
     if (name == "stlmersenne") {
         return STLMersenne::getInstance();
-    } else if (name == "well512a") {
-        return Well512A::getInstance();
     } else {
         THROW("Unknown generator " << name);
     }
