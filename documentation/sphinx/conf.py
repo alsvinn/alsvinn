d0acccfc56f87eb34ffbd00745a98a04060a994c
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2019-06-20 19:33:01 +0200

    version bump

--- documentation/sphinx/conf.py
+++ documentation/sphinx/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Kjetil Olsen Lye ETH Zurich'
 author = 'Kjetil Olsen Lye ETH Zurich'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.1'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
